63f90174cc80cf34ae76effbb072e454cffca983
Soren <sorenisme@gmail.com>
2022-09-20 13:37:55 -0400

    commit tree de27828fc57b5e8851bfdc0282ab65c62ca72c4e

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+// code your solution her
+function saturdayFun( part1, activity = 'roller-skate'){
+    // const part1 = "This Saturday, I want to"
+    return function(){
+        console.log(`${part1} ${activity}`)
+    };
+}
+
+saturdayFun();
